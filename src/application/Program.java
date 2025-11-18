bcdff040879dc764098f4c3e7d9f77a9a01a95bd
cleitonfratoni <cleitonfratoni24@gmail.com>
2025-11-18 02:44:16 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,26 +5,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-import model.entities.Resevation;
+import model.entities.Reservation;
+import model.exception.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in resevation: Check-out date must be after check-in date");
-		} else {
-			Resevation reservation = new Resevation(number, checkIn, checkOut);
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+	
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Resevation: " + reservation);
 			
 			System.out.println();
@@ -34,14 +34,15 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in reservation: " + error);
-			} else {
-				System.out.println("Resevation: " + reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);		
+		} catch (ParseException e) {
+			System.out.println("Invalid date format!");
+		} catch (DomainException e) {
+			System.out.println("Error in resevervation: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
 		}
-		
+
 		sc.close();
 	}
 
